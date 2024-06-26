7b1b79d7509de0e535c2cc5f8c99293018ebc1c3
Elijah M. Immer <me@eimmer.me>
2024-06-26 14:26:46 -0700

    Start of matrix ops.
    Added a way to create uniform matrices addition, subtraction,
    scalar multiplication/division

--- src/matrix.zig
+++ src/matrix.zig
@@ -1,23 +1,99 @@
-pub fn Matrix(comptime n: u32, comptime m: u32) type {
+pub fn Matrix(comptime n: u32, comptime m: u32, comptime t: type) type {
     return struct {
-        rows: [n]@Vector(m, f64),
+        rows: [n]@Vector(m, t),
 
-        pub fn add(lhs: @This(), rhs: @This()) @This() {
-            var rows = .{@splat(0)} ** m;
-
-            for (0.., &rows) |idx, row| {
-                row = lhs.rows[idx] + rhs.rows[idx];
-            }
-
+        pub fn uniform(scalar: t) @This() {
             return .{
-                .rows = rows,
+                .rows = .{@as(@Vector(n, t), @splat(scalar))} ** m,
             };
+        }
+
+        pub fn add(lhs: *@This(), rhs: *const @This()) void {
+            for (&lhs.rows, rhs.rows) |*l, r| {
+                l.* += r;
+            }
+        }
+
+        pub fn sub(lhs: *@This(), rhs: *const @This()) void {
+            for (&lhs.rows, rhs.rows) |*l, r| {
+                l.* -= r;
+            }
+        }
+
+        pub fn mul_scalar(lhs: *@This(), scalar: t) void {
+            const scalar_mul = @as(@Vector(n, t), @splat(scalar));
+
+            for (&lhs.rows) |*l| {
+                l.* *= scalar_mul;
+            }
+        }
+
+        pub fn div_scalar(lhs: *@This(), scalar: t) void {
+            const scalar_vec = @as(@Vector(n, t), @splat(scalar));
+
+            for (&lhs.rows) |*l| {
+                l.* /= scalar_vec;
+            }
         }
     };
 }
 
+test "Matrix Uniform" {
+    const a = Matrix(10, 10, f64).uniform(5);
+    assert(meta.eql(a.rows, .{.{5} ** 10} ** 10));
+}
+
+test "Matrix Add" {
+    const t = Matrix(10, 10, f64);
+
+    var a = t.uniform(5);
+    var b = t.uniform(9);
+
+    const a_clone = a;
+    a.add(&b);
+    b.add(&a_clone);
+
+    assert(meta.eql(a, b));
+}
+
+test "Matrix Sub" {
+    const t = Matrix(10, 10, f64);
+
+    var a = t.uniform(5);
+    var b = t.uniform(9);
+
+    const a_clone = a;
+    a.sub(&b);
+    b.sub(&a_clone);
+
+    assert(meta.eql(a, t.uniform(-4)));
+    assert(meta.eql(b, t.uniform(4)));
+}
+
+test "Matrix Scalar Multiply + Divide" {
+    const t = Matrix(10, 10, f64);
+
+    var a = t.uniform(5);
+    var b = t.uniform(5);
+
+    a.mul_scalar(5);
+    b.div_scalar(5);
+
+    assert(meta.eql(a, t.uniform(25)));
+    assert(meta.eql(b, t.uniform(1)));
+
+    a.div_scalar(5);
+    b.mul_scalar(5);
+
+    assert(meta.eql(a, t.uniform(5)));
+    assert(meta.eql(a, b));
+}
+
 test {
-    std.testing.TestAllDecls(@This());
+    std.testing.refAllDecls(@This());
 }
 
 const std = @import("std");
+const testing = std.testing;
+const assert = std.debug.assert;
+const meta = std.meta;
